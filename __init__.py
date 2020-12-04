4166d556388e787d5da59c555d1396e57757bad3
darksun27 <sbsiddharth@gmail.com>
2020-12-04 15:41:00 +0530

    kuch to hai

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ loaded_model = pickle.load(open("pima.pickle.dat", "rb"))
 
 
 HOST_ADDRESS = "0.0.0.0"
-HOST_ADDRESS = "127.0.0.1"
+# HOST_ADDRESS = "127.0.0.1"
 
 
 GRAPH_DATA_RATE = 0.1 # in seconds
