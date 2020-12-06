dbdafa185fbd7212599bc519bf789d88d7701371
Siddharth Batra <sbsiddharth@gmail.com>
2020-12-06 11:06:25 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ def sendMessage() :
     message = client.messages.create(
             to= '+919958470889',
             from_= '+19183794343',
-            body= 'Yahan mara padha hun mein bsdk!'
+            body= 'Find me here.'
         )
     print(message.sid)
 
